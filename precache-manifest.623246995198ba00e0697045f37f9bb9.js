9e1c7e2a73375cc4810e093e162c53239f6b635e
Shaung Cheng <shaungc@umich.edu>
2019-03-01 22:29:22 -0500

    Updates

---
+++ precache-manifest.623246995198ba00e0697045f37f9bb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/appl-tracky-spa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "372185f45f9030a54e4f",
+    "url": "/appl-tracky-spa/static/js/runtime~main.372185f4.js"
+  },
+  {
+    "revision": "6a81ee33a526ce2d7e09",
+    "url": "/appl-tracky-spa/static/js/main.6a81ee33.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/appl-tracky-spa/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "6a81ee33a526ce2d7e09",
+    "url": "/appl-tracky-spa/static/css/main.8b6ba1ba.chunk.css"
+  },
+  {
+    "revision": "65bfb7e49c9fc70e930639272597908a",
+    "url": "/appl-tracky-spa/index.html"
+  }
+];
