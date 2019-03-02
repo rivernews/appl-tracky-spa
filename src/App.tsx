6220651cdf82d43fbcd15b46d3ebc8db53181a41
Shaung Cheng <shaungc@umich.edu>
2019-03-01 22:33:21 -0500

    scaffold project

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            Hey guys! Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
             className="App-link"
