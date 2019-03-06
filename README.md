c9f12eb11b78eed40d5a30b2a551d556ee1a48a8
Shaung Cheng <shaungc@umich.edu>
2019-03-05 22:56:51 -0500

    testing social auth w/ backend

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Reference
 
-- [This repo](https://github.com/rivernews/appl-tracky)
-    - [Live production website](https://rivernews.github.io/appl-tracky/)
-    - [Deploy repo to github as a live website using gh-pages](https://github.com/gitname/react-gh-pages)
-- [The backend api repo](https://github.com/rivernews/appl-tracky-api)
-- [How to use create-react-app](https://github.com/facebook/create-react-app)
+Repos
+- [This repo](https://github.com/rivernews/appl-tracky-spa)
+- [The backend api repo](https://github.com/rivernews/appl-tracky-spa)
+
+Production sites
+- [Live production website](https://rivernews.github.io/appl-tracky-spa/)
+- [Live backend endpoint](https://appl-tracky-api-https.shaungc.com/)
 
 Technologies
 
+- [How to use create-react-app](https://github.com/facebook/create-react-app)
 - [**Use typescript with React**](https://alligator.io/react/typescript-with-react/#create-react-app-and-typescript) while using `create-react-app` to scaffold the project.
+- [Deploy repo to github as a live website using gh-pages](https://github.com/gitname/react-gh-pages)
+    - If you got a 404 page, and you made sure your homepage url is correct in `package.json`, chances are the page is updating so may need some time reflecting the new deployment. In short: be faithful & patient!
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,111 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { GoogleLogin } from "react-google-login";
+import { GoogleLogout } from "react-google-login";
 
 class App extends Component {
+  state = {
+    clientID: `732988498848-vuhd6g61bnlqe372i3l5pbpnerteu6na.apps.googleusercontent.com`,
+    code: ``,
+    jwtUrl: `http://localhost:8000/api/login/social/jwt_user/`,
+    redirectUri: `postmessage`,
+  };
+
+  componentDidMount() {}
+
+  responseGoogle = (res: any) => {
+    console.log("social provider res is", res);
+    this.setState({
+      code: res.code
+    });
+  };
+
+  onClickRequestServerJWT = () => {
+    console.log(`Ready to request our backend server...`);
+    this.requestServerJWT({
+      code: this.state.code,
+    });
+  };
+
+  requestServerJWT({ code = "" }: { code?: string; redirect_uri?: string }) {
+    // destructuring in typescript: https://mariusschulz.com/blog/typing-destructured-object-parameters-in-typescript
+    this.postRequest(this.state.jwtUrl, {
+      code,
+      provider: "google-oauth2",
+      redirect_uri: this.state.redirectUri,
+    }).then(res => {
+      console.log("our server res is", res);
+    })
+    .catch((err) => {
+        console.error("Gor error when requesting server:", err);
+    });
+  }
+
+  logout = () => {
+    console.log("logged out.");
+    this.setState({
+        code: ``
+    })
+  };
+
+  async postRequest(url: string, data: any = {}) {
+    // Fetch API: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    console.log(`hitting our server (POST) with data:`, data);
+    try {
+      let res = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data) // must match content type in header
+      });
+      return res.json();
+    } catch (err) {
+      console.error("postRequest error:", err);
+    }
+  }
+
   render() {
+    // fetch("https://appl-tracky-api-https.shaungc.com/", {mode: 'cors'})
+    // .then((res) => {
+    //     return res.text();
+    // })
+    // .catch((err) => {
+    //     console.error(err);
+    // })
+    // .then((text) => {
+    //     console.info(text);
+    // });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hey guys! Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Your current client ID:</h1>
+        <p>
+          <input
+            value={this.state.clientID}
+            onChange={event => this.setState({ clientID: event.target.value })}
+          />
+        </p>
+
+        <h1>Current code:</h1>
+        <p>{this.state.code || "(empty)"}</p>
+        <GoogleLogin
+          clientId={this.state.clientID}
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          responseType="code"
+          isSignedIn={false}
+          redirectUri={this.state.redirectUri}
+        />
+        <button onClick={this.onClickRequestServerJWT}>Ask Server JWT</button>
+        <GoogleLogout
+          buttonText="Logout"
+          onLogoutSuccess={this.logout}
+        />
       </div>
     );
   }
