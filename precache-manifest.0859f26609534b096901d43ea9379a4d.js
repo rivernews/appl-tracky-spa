b310f77c6fe955d2ec38084004c7849f99837304
Shaung Cheng <shaungc@umich.edu>
2019-04-07 17:55:48 -0400

    Updates

--- precache-manifest.0859f26609534b096901d43ea9379a4d.js
+++ precache-manifest.0859f26609534b096901d43ea9379a4d.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/appl-tracky-spa/static/js/runtime~main.372185f4.js"
   },
   {
-    "revision": "02aaba55b00274095956",
-    "url": "/appl-tracky-spa/static/js/main.02aaba55.chunk.js"
+    "revision": "6d321de6e4b2ea4d2f5f",
+    "url": "/appl-tracky-spa/static/js/main.6d321de6.chunk.js"
   },
   {
     "revision": "029cd597423d443c6819",
     "url": "/appl-tracky-spa/static/js/2.029cd597.chunk.js"
   },
   {
-    "revision": "02aaba55b00274095956",
+    "revision": "6d321de6e4b2ea4d2f5f",
     "url": "/appl-tracky-spa/static/css/main.6a5ae9d7.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/appl-tracky-spa/static/css/2.e47fd6e0.chunk.css"
   },
   {
-    "revision": "0bc0f141e08ab8762c36c276257ff3b3",
+    "revision": "2ea2baf79be988ad1c545f92acf73ebd",
     "url": "/appl-tracky-spa/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/appl-tracky-spa/precache-manifest.94357bcc19d060cfa392d61cf4720fb9.js"
+  "/appl-tracky-spa/precache-manifest.0859f26609534b096901d43ea9379a4d.js"
 );
 
 workbox.clientsClaim();
