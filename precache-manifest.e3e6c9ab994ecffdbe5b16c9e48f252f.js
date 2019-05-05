5bc42f65baf85c6edf788b73807fb3d66bf765b3
Shaung Cheng <shaungc@umich.edu>
2019-05-04 20:28:50 -0400

    Updates

--- precache-manifest.e3e6c9ab994ecffdbe5b16c9e48f252f.js
+++ precache-manifest.e3e6c9ab994ecffdbe5b16c9e48f252f.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/appl-tracky-spa/static/js/runtime~main.372185f4.js"
   },
   {
-    "revision": "b90e6a17c81af055c319",
-    "url": "/appl-tracky-spa/static/js/main.b90e6a17.chunk.js"
+    "revision": "3e03d66e53c768f3b1a5",
+    "url": "/appl-tracky-spa/static/js/main.3e03d66e.chunk.js"
   },
   {
-    "revision": "3ae145d8711abd20091f",
-    "url": "/appl-tracky-spa/static/js/2.3ae145d8.chunk.js"
+    "revision": "58bbcf40dfa02c9a454f",
+    "url": "/appl-tracky-spa/static/js/2.58bbcf40.chunk.js"
   },
   {
-    "revision": "b90e6a17c81af055c319",
+    "revision": "3e03d66e53c768f3b1a5",
     "url": "/appl-tracky-spa/static/css/main.6a5ae9d7.chunk.css"
   },
   {
-    "revision": "3ae145d8711abd20091f",
+    "revision": "58bbcf40dfa02c9a454f",
     "url": "/appl-tracky-spa/static/css/2.e47fd6e0.chunk.css"
   },
   {
-    "revision": "0251511a1d3df6b728497a8c942c4242",
+    "revision": "2c2b1c49be4a2f1ac56b0896b6e38501",
     "url": "/appl-tracky-spa/index.html"
   }
 ];
