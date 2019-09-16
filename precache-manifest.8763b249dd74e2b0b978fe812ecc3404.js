5859a842fc52966d4238992df77ba234783cf98e
Shaung <shaungc@umich.edu>
2019-09-15 18:15:18 -0700

    Updates

---
+++ precache-manifest.8763b249dd74e2b0b978fe812ecc3404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0e6489e92c28a38a5d3",
+    "url": "/static/css/main.fa317dfb.chunk.css"
+  },
+  {
+    "revision": "b0e6489e92c28a38a5d3",
+    "url": "/static/js/main.b0e6489e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "78bc52fa98cadfdd631a",
+    "url": "/static/css/2.a7b50abf.chunk.css"
+  },
+  {
+    "revision": "78bc52fa98cadfdd631a",
+    "url": "/static/js/2.78bc52fa.chunk.js"
+  },
+  {
+    "revision": "9d4eb2e72b15d5fc523d319dc4d5060d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/appl-tracky-spa/precache-manifest.fd231eb3520b9ca13804769b03e3382f.js"
+  "/precache-manifest.8763b249dd74e2b0b978fe812ecc3404.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/appl-tracky-spa/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
