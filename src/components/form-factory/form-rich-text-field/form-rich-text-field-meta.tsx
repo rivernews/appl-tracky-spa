852f0b13eb1be9fa0f9d58c75c403ccf1ac8c020
Shaung <shaungc@umich.edu>
2019-09-15 18:35:14 -0700

    commit files for #21; setup custom domain

---
+++ src/components/form-factory/form-rich-text-field/form-rich-text-field-meta.tsx
@@ -0,0 +1,21 @@
+// base field
+import { FormBaseFieldMeta, IFormBaseFieldProps, IFieldBaseMetaProps } from "../form-base-field/form-base-field-meta";
+// input field
+import { FormRichTextField } from "./form-rich-text-field";
+
+
+// API for caller to new props for input field
+export interface IFormRichTextFieldProps extends IFormBaseFieldProps {
+}
+
+// for defining meta
+export interface IFormRichTextFieldMetaProps extends IFieldBaseMetaProps {
+}
+
+export class FormRichTextFieldMeta extends FormBaseFieldMeta {
+
+    constructor(props: IFormRichTextFieldMetaProps) {
+        super(props)
+        this.formField = FormRichTextField;
+    }
+}
---
+++ src/components/form-factory/form-rich-text-field/form-rich-text-field.tsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+/** Components */
+// mdc react icon
+import MaterialIcon from "@material/react-material-icon";
+// mdc react button
+import "@material/react-button/dist/button.css";
+// mdc-react input
+import "@material/react-text-field/dist/text-field.css";
+import TextField, { Input } from "@material/react-text-field";
+// formik
+import {
+    Field, FieldProps,
+    ErrorMessage,
+} from "formik";
+// input field
+import { IFormRichTextFieldProps } from "./form-rich-text-field-meta";
+// ckeditor
+import CKEditor from '@ckeditor/ckeditor5-react';
+import BalloonEditor from '@ckeditor/ckeditor5-build-balloon';
+
+export class FormRichTextField extends Component<IFormRichTextFieldProps> {
+    render() {
+        return (
+            <div className="FormRichTextField">
+                <Field
+                    name={this.props.fieldName}
+                    render={({ field, form }: FieldProps<number | string>) => {
+                        return (
+                        <div className="RichTextFieldInput">
+                            <div><strong>{this.props.label}</strong></div>
+                            <CKEditor
+                                editor={BalloonEditor}
+                                data={field.value}
+                                onChange={ ( event: any, editor: any ) => {
+                                    form.setFieldValue(field.name, editor.getData());
+                                } }
+                            />
+                        </div>
+                    )}}
+                />
+                <ErrorMessage name={this.props.fieldName} />
+            </div>
+        );
+    }
+}
