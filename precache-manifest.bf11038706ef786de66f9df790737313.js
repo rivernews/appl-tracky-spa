ddc6e95978461e517ba5f22544e68eb719c4870f
Shaung <shaungc@umich.edu>
2019-10-13 22:37:51 -0700

    Updates

---
+++ precache-manifest.bf11038706ef786de66f9df790737313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa7dcb283b2a45ca21f2",
+    "url": "/static/css/main.f078d2dc.chunk.css"
+  },
+  {
+    "revision": "aa7dcb283b2a45ca21f2",
+    "url": "/static/js/main.aa7dcb28.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "030dd43f7f50ae109e27",
+    "url": "/static/css/2.27cf1229.chunk.css"
+  },
+  {
+    "revision": "030dd43f7f50ae109e27",
+    "url": "/static/js/2.030dd43f.chunk.js"
+  },
+  {
+    "revision": "81c35f58777a8a986579cf175f11b2d3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b65a498db2752faf75236e5f9d75b6b.js"
+  "/precache-manifest.bf11038706ef786de66f9df790737313.js"
 );
 
 workbox.clientsClaim();
