52cf1700f2f46d5c2d35dc05b3964aef1b0ea5dc
Shaung Cheng <shaungc@umich.edu>
2020-12-06 01:21:03 -0800

    Updates

---
+++ precache-manifest.45b6facd4f7ccfb6b6885b8428b66353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9179e2af5bbdaaf5ab0c69acf89a70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "822e02f0efe70473ce02",
+    "url": "/static/css/2.c73e2944.chunk.css"
+  },
+  {
+    "revision": "26809243a9c49b40afc4",
+    "url": "/static/css/main.61472147.chunk.css"
+  },
+  {
+    "revision": "822e02f0efe70473ce02",
+    "url": "/static/js/2.aee5b489.chunk.js"
+  },
+  {
+    "revision": "6e2d41c3473601737fd86100f4a98636",
+    "url": "/static/js/2.aee5b489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26809243a9c49b40afc4",
+    "url": "/static/js/main.46839b5f.chunk.js"
+  },
+  {
+    "revision": "18a18ac54703c81429f6",
+    "url": "/static/js/runtime-main.f7861192.js"
+  }
+]);
